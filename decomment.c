6759b58bd3eeff0bd9a3afc0718836a9e1e9266c
ludancom <la9361@princeton.edu>
2025-09-18 03:15:14 -0400

    final version inshallah

--- decomment.c
+++ decomment.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+/* defines constants representing each state in the DFA */
 enum Statetype {START, BACKSLASH, STRING_QUOTE_ONE, CHAR_QUOTE_ONE, STAR_ONE, 
 STRING_QUOTE_AFTER_STAR_ONE, CHAR_QUOTE_AFTER_STAR_ONE, STAR_TWO, ESCAPED_STRING_ONE,
 ESCAPED_CHAR_ONE, ESCAPED_STRING_TWO, ESCAPED_CHAR_TWO};
 
-static int lineNumber = 1;
-static int lineNumberInComment = 0;
+static int lineNumber = 1; /* counts the number of lines outside of an unterminated comment */
+static int lineNumberInComment = 0; /* counts the number of lines inside of a terminated comment */
+
+/*
+handles the start state function. c is the current character of the DFA.
+writes c to stdout when appropriate, and moves to other states when necessary.
+*/
 enum Statetype handleStartState(char c){
     enum Statetype state;
     if(c == '/'){
@@ -31,6 +37,10 @@ enum Statetype handleStartState(char c){
     return state;
 }
 
+/*
+handles the state function for after a backslash is read. c is the current character
+of the DFA. writes c to stdout when appropriate, and moves to other states when necessary.
+*/
 enum Statetype handleBackslashState(char c){
     enum Statetype state;
     if(c == '*'){
@@ -62,6 +72,10 @@ enum Statetype handleBackslashState(char c){
     return state;
 }
 
+/*
+implements the string literal state for before we are a state of within the comment
+connected to start and backslash states.
+*/
 enum Statetype handleStringQuoteOneState(char c){
     enum Statetype state;
     if (c == '"'){
@@ -80,6 +94,10 @@ enum Statetype handleStringQuoteOneState(char c){
     return state;
 }
 
+/*
+handles escaped characters within strings when we are not in an in 
+comment state.
+*/
 enum Statetype handleEscapedStringOne(char c) {
     enum Statetype state;
     putchar(c);
@@ -87,6 +105,10 @@ enum Statetype handleEscapedStringOne(char c) {
     return state;            
 }
 
+/*
+implements the char literal state for before we are a state of 
+within the comment connected to start and backslash states.
+*/
 enum Statetype handleCharQuoteOneState(char c){
     enum Statetype state;
     if (c == '\''){
@@ -105,7 +127,10 @@ enum Statetype handleCharQuoteOneState(char c){
     return state;
 }
 
-
+/*
+handles escaped characters within character literals when we are 
+not in an in comment state.
+*/
 enum Statetype handleEscapedCharOne(char c) {
     enum Statetype state;
     putchar(c);
@@ -113,7 +138,11 @@ enum Statetype handleEscapedCharOne(char c) {
     return state;            
 }
 
-
+/*
+implements state after a star is read in (right after a backslash),
+in otherwords, implements the state of within the comment. lineNumberInComment
+begins counting within here.
+*/
 enum Statetype handleStarOneState(char c){
     enum Statetype state;
     if(c == '*'){
@@ -135,6 +164,9 @@ enum Statetype handleStarOneState(char c){
     return state;
 }
 
+/*
+implements the string literal state for when we are the state within the comment.
+*/
 enum Statetype handleStringQuoteAfterStarOneState(char c){
     enum Statetype state;
     if (c == '"'){
@@ -152,6 +184,11 @@ enum Statetype handleStringQuoteAfterStarOneState(char c){
     return state;
 }
 
+/*
+handles escaped characters within string literals when we are 
+in an in comment state. Handles the next character after the
+backslash by treating it ordinarily and writing it to stdout.
+*/
 enum Statetype handleEscapedStringTwo(char c) {
     enum Statetype state;
     putchar(c);
@@ -159,6 +196,9 @@ enum Statetype handleEscapedStringTwo(char c) {
     return state;            
 }
 
+/*
+implements the char literal state for when we are the state within the comment.
+*/
 enum Statetype handleCharQuoteAfterStarOneState(char c){
     enum Statetype state;
     if (c == '\''){
@@ -176,6 +216,11 @@ enum Statetype handleCharQuoteAfterStarOneState(char c){
     return state;
 }
 
+/*
+handles escaped characters within char literals when we are 
+in an in comment state. Handles the next character after the
+backslash by treating it ordinarily and writing it to stdout.
+*/
 enum Statetype handleEscapedCharTwo(char c) {
     enum Statetype state;
     putchar(c);
@@ -183,7 +228,11 @@ enum Statetype handleEscapedCharTwo(char c) {
     return state;            
 }
 
-
+/*
+implements the state of when we reach a second star in the code.
+if a backslash is detected right after, it's sent to the start state,
+otherwise we are sent to the state deemed by the rest of the DFA
+*/
 enum Statetype handleStarTwoState(char c){
     enum Statetype state;
     if(c == '/'){
@@ -210,7 +259,11 @@ enum Statetype handleStarTwoState(char c){
 }
 
 
-
+/*
+reads characters one by one from stdin until the end of the file 
+is reached. starts at Start state then switches based on the DFA.
+Failure error is outputted at the presence of unterminated comments.
+*/
 
 int main(void)
 {
@@ -259,11 +312,12 @@ int main(void)
         }
     }
 
+    /* first if statement includes all accepted states*/
     if(state == START || state == BACKSLASH 
     || state == STRING_QUOTE_ONE || state == CHAR_QUOTE_ONE 
     || state == ESCAPED_CHAR_ONE || state == ESCAPED_STRING_ONE){
         if(state == BACKSLASH){
-            putchar('/');
+            putchar('/'); /* handles the edge case of ending on a backslash in an accepted state */
         }
         return EXIT_SUCCESS;
     }
